c35a4fbca886bc09bf693cf25c5cea9477c8842d
ALinuxPerson <alinuxperson@gmail.com>
2024-06-12 10:55:06 +0800

    Add more cargo packages

--- alp-toolkit/src/lib.rs
+++ alp-toolkit/src/lib.rs
@@ -1,2 +1,11 @@
 #[cfg(feature = "log")]
 pub extern crate alptk_log as log;
+
+#[cfg(feature = "config")]
+pub extern crate alptk_config as config;
+
+#[cfg(feature = "location")]
+pub extern crate alptk_location as location;
+
+#[cfg(feature = "confloc")]
+pub extern crate alptk_config_location as confloc;
